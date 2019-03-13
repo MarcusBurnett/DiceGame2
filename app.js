05a1f8f17ca1119f780efdefa2499da4be5bbce7
MarcusBurnett <marco95bwfc@gmail.com>
2019-03-13 11:52:21 +0000

    created seperate functions for each section

--- app.js
+++ app.js
@@ -11,12 +11,17 @@ var roll = document.getElementById('roll');
 var hold = document.getElementById('hold');
 var die = document.getElementById('img');
 var gameOver = false;
-var roundOver = false;
 var p1Turn = true;
 var limit = 20;
 
 
-reset.addEventListener('click', function(){
+reset.addEventListener('click', resetGame);
+
+roll.addEventListener('click', rollDie);
+
+hold.addEventListener('click', holdDie);
+
+function resetGame(){
     player1.textContent = 'Player 1';
     player2.textContent = 'Player 2';
     player1Score = 0;
@@ -36,37 +41,98 @@ reset.addEventListener('click', function(){
     p2Section.classList.remove('active');
     document.getElementById('score-0').textContent = 0;
     document.getElementById('score-1').textContent = 0;
-})
+}
 
-roll.addEventListener('click', function (){
+function rollDie(){
     if(!gameOver){
         var rollScore = Math.floor(Math.random()* 6) + 1;
             if(p1Turn){
-                die.style.backgroundImage = "url('img/dice" + rollScore + ".png')";
-                player1CurrentScore += rollScore;
-                document.getElementById('current-score-0').textContent = player1CurrentScore;
-
-                if(rollScore === 1){
-                    player1CurrentScore = 0;
-                    p1Turn = false;
-                    p1Section.classList.toggle('active');
-                    p2Section.classList.toggle('active');
-                    document.getElementById('current-score-0').textContent = 0;
-                };
-                
-                if (player1CurrentScore >= limit){
-                    player1.textContent = "WINNER";
-                    document.getElementById('score-0').textContent = player1CurrentScore + player2Score;
-                    p1Section.classList.add('active');
-                    p2Section.classList.remove('active');
-                    p2Section.style.opacity = '0.5';
-                    roll.classList.add('hidden');
-                    hold.classList.add('hidden');
-                    gameOver = true;
-                };
-
+                p1Roll(rollScore);
             } else {
-                    die.style.backgroundImage = "url('img/dice" + rollScore + ".png')";
+                p2Roll(rollScore);
+            } 
+    }
+}
+
+function holdDie(){
+    if(!gameOver){
+
+        if(p1Turn){
+            p1Hold();
+
+        } else {
+            p2Hold();
+        }
+    }
+}
+
+
+function p1Hold(){
+    player1Score += player1CurrentScore;
+    document.getElementById('score-0').textContent = player1Score;
+    player1CurrentScore = 0;
+    p1Section.classList.toggle('active');
+    p2Section.classList.toggle('active');
+    document.getElementById('current-score-0').textContent = 0;
+
+    if (player1Score >= limit){
+        player1.textContent = "WINNER";
+        p2Section.classList.remove('active');
+        p1Section.classList.add('active');
+        p2Section.style.opacity = '0.5';
+        roll.classList.add('hidden');
+        hold.classList.add('hidden');
+        gameOver = true;
+    } 
+    p1Turn = false;
+}
+
+function p2Hold(){
+    player2Score += player2CurrentScore;
+    document.getElementById('score-1').textContent = player2Score;
+    player2CurrentScore = 0;
+    p1Section.classList.toggle('active');
+    p2Section.classList.toggle('active');
+    document.getElementById('current-score-1').textContent = 0;
+    if (player2Score >= limit){
+        player2.textContent = "WINNER";
+        p2Section.classList.add('active');
+        p1Section.classList.remove('active');
+        p1Section.style.opacity = '0.5';
+        roll.classList.add('hidden');
+        hold.classList.add('hidden');
+        gameOver = true;
+    }
+    p1Turn = true;
+}
+
+function p1Roll(rollScore){
+    die.style.backgroundImage = "url('img/dice" + rollScore + ".png')";
+    player1CurrentScore += rollScore;
+    document.getElementById('current-score-0').textContent = player1CurrentScore;
+
+    if(rollScore === 1){
+        player1CurrentScore = 0;
+        p1Turn = false;
+        p1Section.classList.toggle('active');
+        p2Section.classList.toggle('active');
+        document.getElementById('current-score-0').textContent = 0;
+    };
+    
+    if (player1CurrentScore >= limit){
+        player1.textContent = "WINNER";
+        document.getElementById('score-0').textContent = player1CurrentScore + player2Score;
+        p1Section.classList.add('active');
+        p2Section.classList.remove('active');
+        p2Section.style.opacity = '0.5';
+        roll.classList.add('hidden');
+        hold.classList.add('hidden');
+        gameOver = true;
+    };
+}
+
+function p2Roll(rollScore){
+    die.style.backgroundImage = "url('img/dice" + rollScore + ".png')";
                     player2CurrentScore += rollScore;
                     document.getElementById('current-score-1').textContent = player2CurrentScore;
 
@@ -90,60 +156,12 @@ roll.addEventListener('click', function (){
                         hold.classList.add('hidden');
                         gameOver = true;    
                     };
-                } 
-    }
-});
+}
 
-hold.addEventListener('click', function (){
-    if(!gameOver){
-
-            if(p1Turn){
-                player1Score += player1CurrentScore;
-                document.getElementById('score-0').textContent = player1Score;
-                player1CurrentScore = 0;
-                p1Section.classList.toggle('active');
-                p2Section.classList.toggle('active');
-                document.getElementById('current-score-0').textContent = 0;
-
-                if (player1Score >= limit){
-                    player1.textContent = "WINNER";
-                    p2Section.classList.remove('active');
-                    p1Section.classList.add('active');
-                    p2Section.style.opacity = '0.5';
-                    roll.classList.add('hidden');
-                    hold.classList.add('hidden');
-                    gameOver = true;
-                }
-                p1Turn = false;
-
-            } else {
-                player2Score += player2CurrentScore;
-                document.getElementById('score-1').textContent = player2Score;
-                player2CurrentScore = 0;
-                p1Section.classList.toggle('active');
-                p2Section.classList.toggle('active');
-                document.getElementById('current-score-1').textContent = 0;
-                if (player2Score >= limit){
-                    player2.textContent = "WINNER";
-                    p2Section.classList.add('active');
-                    p1Section.classList.remove('active');
-                    p1Section.style.opacity = '0.5';
-                    roll.classList.add('hidden');
-                    hold.classList.add('hidden');
-                    gameOver = true;
-                }
-                p1Turn = true;
-            }
-        
-    }
-})
-
-// let vh = window.innerHeight * 0.01;
-// // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
-    // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
+
+
